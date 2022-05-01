67a838f947ea80f9088d049aca3527d26d8d67af
Sebastian Heng <sh843@uowmail.edu.au>
2022-05-02 02:42:10 +0800

    commiting mdf and UEN

--- WebApplication1/customerUEN.aspx.cs
+++ WebApplication1/customerUEN.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -16,7 +18,28 @@ namespace WebApplication1
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
-            Response.Redirect("customerOrder.aspx");
+            string inputVal = txtUEN.Text.Trim();
+            bool intValid = false;
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["LoginConnectionString"].ConnectionString);
+            con.Open();
+            string query_table = "SELECT count(*) FROM [dbo].[Table] WHERE uen= @uen";
+            SqlCommand cmd = new SqlCommand(query_table, con);
+            cmd.Parameters.AddWithValue("@uen", inputVal);
+            if (Int32.TryParse(cmd.ExecuteScalar().ToString(), out int executedInt))
+            {
+                if (executedInt == 1)
+                {
+                    intValid = true;
+                }
+            }
+            if (intValid == false)
+            {
+                Response.Write("<script>alert('Invalid UEN! Please reenter the UEN.')</script>");
+            }
+            else
+            {
+                Response.Redirect("customerOrder.aspx");
+            }
         }
     }
 }
